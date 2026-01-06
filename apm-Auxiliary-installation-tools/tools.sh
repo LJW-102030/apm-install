5b95f55b8509c9621e134ac1a92f1cc4c6f887a3
LJW-102030 <linjunwei.yishanjun@gmail.com>
2026-01-06 20:55:32 +0800

    Update tools.sh

--- apm-Auxiliary-installation-tools/tools.sh
+++ apm-Auxiliary-installation-tools/tools.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+repo="https://raw.githubusercontent.com/LJW-102030/apm-install/refs/heads/main/"
+
+Testrepo() {
+  code=$(curl -s -o /dev/null -w "$repo")
+  if [ "$code" != 404 ]; then
+    echo "Ok."
+  else
+    echo "repo error."
+  fi
+}
+
+onlinemv() {
+  
+
+case "$1" in
+  -onlinemv) onlinemv
+             ;;
+  -Testrepo) Testrepo
+             ;;
+esac
